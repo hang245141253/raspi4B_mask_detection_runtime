09e2174bd594475a0d60438fbd6fe19666a2488f
hang245141253 <hang245141253@gmail.com>
2020-06-16 23:07:06 +0800

    更新了v2.6.0 32位模型库

--- code/cmake.sh
+++ code/cmake.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # configure
-TARGET_ARCH_ABI=armv8 # for RK3399, set to default arch abi
-#TARGET_ARCH_ABI=armv7hf # for Raspberry Pi 3B
+TARGET_ARCH_ABI=armv8 # for RK3399, set to default arch abi 等64位系统
+#TARGET_ARCH_ABI=armv7hf # for Raspberry Pi 3B 或者32位官方系统的树苺派4B
 PADDLE_LITE_DIR=../Paddle-Lite
 
 
--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # configure
-TARGET_ARCH_ABI=armv8 # for RK3399, set to default arch abi
-#TARGET_ARCH_ABI=armv7hf # for Raspberry Pi 3B
+#TARGET_ARCH_ABI=armv8 # for RK3399, set to default arch abi
+TARGET_ARCH_ABI=armv7hf # for Raspberry Pi 3B
 PADDLE_LITE_DIR=../Paddle-Lite
 
 #run
