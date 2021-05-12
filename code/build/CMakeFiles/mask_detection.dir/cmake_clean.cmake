286d3cb8b9a137e6770f41dff46a4de0821ab9b4
hang245141253 <hang245141253@gmail.com>
2021-05-12 04:10:33 +0000

    update Paddle-Lite-2.8 libs and models

---
+++ code/build/CMakeFiles/mask_detection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mask_detection.dir/mask_detection.cc.o"
+  "mask_detection.pdb"
+  "mask_detection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mask_detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
